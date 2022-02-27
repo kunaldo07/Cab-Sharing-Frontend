2ca4652449ae1ef029b8d9573e1649d0e056531c
Kunal <kunalbadole5@gmail.com>
2022-02-27 14:33:45 +0530

    First commit

---
+++ client-side/src/components/details/Details.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import DetailsBlock from '../detailsBlock/DetailsBlock';
+import "./details.css";
+
+const Details = () => {
+  return (
+    <div className="details">
+        <div className="details-element">
+            <DetailsBlock/>
+        </div>
+    </div>
+  )
+}
+
+export default Details
---
+++ client-side/src/components/topbar/Topbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./topbar.css";
+
+const Topbar = () => {
+  return (
+    <div className="top">
+        <div className="heading">
+          IIT Hyderabad Cab Sharing
+        </div>
+    </div>
+  )
+}
+
+export default Topbar;
